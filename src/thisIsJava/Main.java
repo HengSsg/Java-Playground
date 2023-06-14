cf45cc62a1335f20f3e7de3cb9147d5f7391f1c7
HyungSeok Park <dnflekf2748@gmail.com>
2023-06-14 19:44:29 +0900

    Create Main.java

---
+++ src/thisIsJava/Main.java
@@ -0,0 +1,10 @@
+package thisIsJava;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][][][][][] a = new int[1][1][1][1][1][1];
+        System.out.println(Arrays.deepToString(a));
+    }
+}
